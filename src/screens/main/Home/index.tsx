e7ee880246635721e66e8a7dfcf4f16686339a36
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2023-12-04 20:01:06 +0530

    feat: added current location

--- src/screens/main/Home/index.tsx
+++ src/screens/main/Home/index.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Button, Text, Modal } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Circle, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 const Home = () => {
   const [markers, setMarkers] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState(null);
 
   const handleMapPress = event => {
     const { coordinate } = event.nativeEvent;
@@ -27,17 +29,53 @@ const Home = () => {
     setModalVisible(false);
   };
 
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        setCurrentLocation({ latitude, longitude });
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    );
+  }, []);
+
+  if (!currentLocation) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <MapView
-        style={{ flex: 1 }}
+        style={{ flex: 1, width: '100%', height: '100%' }}
+        provider={PROVIDER_GOOGLE}
         onLongPress={handleMapPress}
+        showsMyLocationButton={true}
+        zoomControlEnabled={true}
         initialRegion={{
-          latitude: 37.7749,
-          longitude: -122.4194,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
         }}>
+        <Circle
+          center={currentLocation}
+          radius={500}
+          strokeColor="rgba(158, 158, 255, 1.0)"
+          fillColor="rgba(158, 158, 255, 0.3)"
+        />
+        <Marker
+          coordinate={currentLocation}
+          title="Current Location"
+          description="This is where you are"
+          pinColor="blue"
+        />
         {markers.map((marker, index) => (
           <Marker
             key={index}
